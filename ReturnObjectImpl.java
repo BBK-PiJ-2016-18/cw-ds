17c09d8355c7066709dbd35b0f4180f48292fdeb
Patrick Hewson <Patrick Hewson>
2016-11-24 00:19:25 +0000

    fixes

--- ReturnObjectImpl.java
+++ ReturnObjectImpl.java
@@ -1,12 +1,12 @@
 public class ReturnObjectImpl implements ReturnObject {/**
- * A wrapper containing either an object (the result of an operation
- * on a data structure) or an error value.
+ * A wrapper containing either an object (the result of an operation on a data structure) or an error value.
  *
 
 	/**
 	 * Returns whether there has been an error
 	 * @return whether there has been an error
 	 */
+	
 	public boolean hasError() {
 		if (getReturnValue() == null)  {
 			return true;
@@ -17,26 +17,22 @@ public class ReturnObjectImpl implements ReturnObject {/**
 	/**
 	 * Returns the error message. 
 	 * 
-	 * This method must return NO_ERROR if and only if
-	 * {@hasError} returns false.
+	 * This method must return NO_ERROR if and only if {@hasError} returns false.
 	 * 
 	 * @return the error message
 	 */
 	public ErrorMessage getError() {
 		if (hasError() == false) {
 			return ErrorMessage.NO_ERROR;
-		}		
+		}
+		return ErrorMessage.EMPTY_STRUCTURE;
 	}
 
 	/**
-	 * Returns the object wrapped in this ReturnObject, i.e. the
-	 * result of the operation if it was successful, or null if
-	 * there has been an error.
+	 * Returns the object wrapped in this ReturnObject, i.e. the result of the operation if it was successful, or null if there has been an error.
 	 * 
-	 * Note that the output of this method must be null if {@see
-	 * hasError} returns true, but the opposite is not true: if
-	 * {@see hasError} returns false, this method may or may not
-	 * return null.
+	 * Note that the output of this method must be null if {@see hasError} returns true, but the opposite is not true: if {@see hasError} returns false, this 
+	 * method may or may not return null.
 	 * 
 	 * @return the return value from the method or null if there has been an error
 	 */
@@ -44,14 +40,21 @@ public class ReturnObjectImpl implements ReturnObject {/**
 		if (hasError() == true) {
 			return null;
 		}
+		return this.obj;
 	}
+	
+	/*
+	* adding a field
+	*/
+	
+	private Object obj;
 	
 	/**
 	* The constructor for when there is no error
 	*/
 	
-	public ReturnObjectImpl(Object obj) {
-		this.getError() = ErrorMessage.NO_ERROR;
+	ReturnObjectImpl(Object obj) {
+		this.obj = obj;
 	}
 	
 	/**
