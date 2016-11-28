3d38b6d11ba0a2b7ff286f412251187b3976c6df
Patrick Hewson <phewso01@dcs.bbk.ac.uk>
2016-11-28 18:56:35 +0000

    fixes

--- ArrayList.java
+++ ArrayList.java
@@ -1,20 +1,12 @@
 /**
- * A list is a collection of objects that are sorted and can be
- * accessed by index. The first element in the list is at index 0.
+ * A list is a collection of objects that are sorted and can be accessed by index. The first element in the list is at index 0.
  *
- * A list can store objects of any kind, and they can be of different
- * types: Integers, Doubles, String, or even other lists. However,
- * this list cannot store null objects.
+ * A list can store objects of any kind, and they can be of different types: Integers, Doubles, String, or even other lists. However, this list cannot store null objects.
  * 
- * There is no limit to the number of elements in the list (provided
- * that there is free memory in the Java Virtual Machine).
+ * There is no limit to the number of elements in the list (provided that there is free memory in the Java Virtual Machine).
  * 
- * Not all operations on a list will always be successful. For
- * example, a programmer may try to remove an element from an empty
- * list, or from a position where there is nothing. Since we hace not
- * covered exceptions yet, we need another mechanism to report
- * errors. In order to do that, methods of this list will return a
- * {@see ReturnObject} that will contain either an object or an error
+ * Not all operations on a list will always be successful. For example, a programmer may try to remove an element from an empty list, or from a position where there is nothing. Since we hace not
+ * covered exceptions yet, we need another mechanism to report errors. In order to do that, methods of this list will return a {@see ReturnObject} that will contain either an object or an error
  * value of the right kind (as defined in {@see ErrorMessage}). 
  * 
  * @author PiJ
@@ -47,12 +39,10 @@ public class ArrayList implements List {
 	/**
 	 * Returns the element at the given position. 
 	 * 
-	 * If the index is negative or greater or equal than the size of
-	 * the list, then an appropriate error must be returned.
+	 * If the index is negative or greater or equal than the size of the list, then an appropriate error must be returned.
 	 * 
 	 * @param index the position in the list of the item to be retrieved
-	 * @return the element or an appropriate error message, 
-	 *         encapsulated in a ReturnObject
+	 * @return the element or an appropriate error message,  encapsulated in a ReturnObject
 	 */
 	public ReturnObjectImpl get(int index) {
 		if (index >= list.length || index < 0) {
@@ -64,16 +54,12 @@ public class ArrayList implements List {
 	}
 
 	/**
-	 * Returns the elements at the given position and removes it
-	 * from the list. The indeces of elements after the removed
-	 * element must be updated accordignly.
+	 * Returns the elements at the given position and removes it from the list. The indeces of elements after the removed element must be updated accordingly.
 	 * 
-	 * If the index is negative or greater or equal than the size of
-	 * the list, then an appropriate error must be returned.
+	 * If the index is negative or greater or equal than the size of the list, then an appropriate error must be returned.
 	 * 
 	 * @param index the position in the list of the item to be retrieved
-	 * @return the element or an appropriate error message, 
-	 *         encapsulated in a ReturnObject
+	 * @return the element or an appropriate error message, encapsulated in a ReturnObject
 	 */
 	public ReturnObjectImpl remove(int index) {
 		if (index < 0 || index > list.length) {
@@ -89,31 +75,25 @@ public class ArrayList implements List {
 			}
 		}
 		if (index < list.length) { 
-			for (int i = index; i < list.length; i++) {
+			for (int i = index; i < newLength; i++) {
 				newList[i] = list[i+1];
 			}
 		}
+		list = newList;
 		ReturnObjectImpl returner = new ReturnObjectImpl(toBeReturned);
 		return returner;
 	}
 
 	/**
-	 * Adds an element to the list, inserting it at the given
-	 * position. The indeces of elements at and after that position
-	 * must be updated accordignly.
+	 * Adds an element to the list, inserting it at the given position. The indeces of elements at and after that position must be updated accordingly.
 	 * 
-	 * If the index is negative or greater or equal than the size of
-	 * the list, then an appropriate error must be returned.
+	 * If the index is negative or greater or equal than the size of the list, then an appropriate error must be returned.
 	 * 
-	 * If a null object is provided to insert in the list, the
-	 * request must be ignored and an appropriate error must be
-	 * returned.
+	 * If a null object is provided to insert in the list, the request must be ignored and an appropriate error must be returned.
 	 * 
-	 * @param index the position at which the item should be inserted in
-	 *              the list
+	 * @param index the position at which the item should be inserted in the list
 	 * @param item the value to insert into the list
-	 * @return an ReturnObject, empty if the operation is successful
-	 *         or containing an appropriate error message otherwise
+	 * @return an ReturnObject, empty if the operation is successful or containing an appropriate error message otherwise
 	 */
 	public ReturnObjectImpl add(int index, Object item) {
 		
@@ -139,10 +119,11 @@ public class ArrayList implements List {
 		newList[index] = item;
 		
 		if (index < list.length) { 
-			for (int i = index; i < list.length; i++) {
+			for (int i = index + 1; i < newLength; i++) {
 				newList[i] = list[i-1];
 			}
 		}
+		list = newList;
 		ReturnObjectImpl emptyReturn = new ReturnObjectImpl(null);
 		return emptyReturn;
 	}
@@ -150,13 +131,10 @@ public class ArrayList implements List {
 	/**
 	 * Adds an element at the end of the list.
 	 * 
-	 * If a null object is provided to insert in the list, the
-	 * request must be ignored and an appropriate error must be
-	 * returned.
+	 * If a null object is provided to insert in the list, the request must be ignored and an appropriate error must be returned.
 	 * 
 	 * @param item the value to insert into the list
-	 * @return an ReturnObject, empty if the operation is successful
-	 *         or containing an appropriate error message otherwise
+	 * @return an ReturnObject, empty if the operation is successful or containing an appropriate error message otherwise
 	 */
 	public ReturnObjectImpl add(Object item) {
 		
@@ -172,7 +150,17 @@ public class ArrayList implements List {
 			newList[i] = list[i];
 		}
 		newList[newLength - 1] = item;
+		list = newList;
 		ReturnObjectImpl emptyReturn = new ReturnObjectImpl(null);
 		return emptyReturn;
+	}
+	
+	public void getList() {
+		System.out.print("{");
+		for (int i = 0; i < list.length; i++) {
+			System.out.print(list[i] + ", ");
+		}
+		System.out.print("}");
+		System.out.println();
 	}
 }
--- ArrayListLauncher.java
+++ ArrayListLauncher.java
@@ -5,13 +5,24 @@ public class ArrayListLauncher {
 		testList.add(2);
 		testList.add(3);
 		testList.add(4);
+		testList.getList();
 		testList.add(5);
 		testList.add(6);
-		System.out.println(ArrayList.list[0]);
-		System.out.println(ArrayList.list[1]);
-		System.out.println(ArrayList.list[2]);
-		System.out.println(ArrayList.list[3]);
-		System.out.println(ArrayList.list[4]);
-		System.out.println(ArrayList.list[5]);
+		testList.add("seven");
+		testList.getList();
+		testList.remove(2);
+		testList.getList();
+		testList.add(8);
+		testList.getList();
+		testList.add(1, "one point five");
+		testList.getList();
+		/** System.out.println(testList.list[0]);
+		System.out.println(testList.list[1]);
+		System.out.println(testList.list[2]);
+		System.out.println(testList.list[3]);
+		System.out.println(testList.list[4]);
+		System.out.println(testList.list[5]);
+		System.out.println(testList.list);
+		*/
 	}
 }
