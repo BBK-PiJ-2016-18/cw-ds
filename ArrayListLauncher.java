1516842844ec93dfd4f72c947d28dea15c0ef987
Patrick Hewson <phewso01@dcs.bbk.ac.uk>
2016-11-28 18:26:52 +0000

    Added tests

---
+++ ArrayListLauncher.java
@@ -0,0 +1,17 @@
+public class ArrayListLauncher {
+	public static void main(String[] args) {
+		ArrayList testList = new ArrayList();
+		testList.add(1);
+		testList.add(2);
+		testList.add(3);
+		testList.add(4);
+		testList.add(5);
+		testList.add(6);
+		System.out.println(ArrayList.list[0]);
+		System.out.println(ArrayList.list[1]);
+		System.out.println(ArrayList.list[2]);
+		System.out.println(ArrayList.list[3]);
+		System.out.println(ArrayList.list[4]);
+		System.out.println(ArrayList.list[5]);
+	}
+}
